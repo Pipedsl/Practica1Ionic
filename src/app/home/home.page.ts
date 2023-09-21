b0b67efc4d2416a7b943a14bf64ce6d985e69b03
Pipedsl <h.navarrete@duocuc.cl>
2023-09-21 15:55:29 -0300

    tercer commit se hace variable y funcion contador

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -6,7 +6,11 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
-
+  public contador: number = 0;
   constructor() {}
+
+  public aumentarContador(){
+    this.contador = this.contador + 1;
+  }
 
 }
